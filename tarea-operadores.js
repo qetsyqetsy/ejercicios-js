7b8dd298a45668692553c3573fd3408d26ff78dd
Qetsy <qetsyqetsy@gmail.com>
2024-02-26 03:19:00 -0300

    tarea de operadores js, versiones 1 y 2

---
+++ tarea-operadores.js
@@ -0,0 +1,13 @@
+/* dadas las siguientes tres variables, 
+escribí un programa que imprima en la consola 
+cuál es el número más grande */
+
+let num1 = 1;
+let num2 = 2; 
+let num3 = 3; 
+
+let biggest = Math.max(num1, num2, num3);
+console.log(biggest)
+
+
+
---
+++ tarea-operadoresv2.js
@@ -0,0 +1,22 @@
+/* dadas las siguientes tres variables, 
+escribí un programa que imprima en la consola 
+cuál es el número más grande */
+
+let num1 = 1;
+let num2 = 2; 
+let num3 = 3; 
+
+if (num1 > num2) {
+	may1 = num1
+} else {
+	may1 = num2 
+}
+
+if (may1 > num3) {
+	may2 = may1
+} else {
+	may2 = num3 
+}
+
+console.log(may2)
+
